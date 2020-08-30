2f0ca836d85fd951590fa0f3a7f9b0369049e76d
rahulk <rahulk232@gmail.com>
2020-08-29 23:33:34 -0400

    Search property

--- lib/screen/property/search.dart
+++ lib/screen/property/search.dart
@@ -11,6 +11,7 @@ class PropertySearchForm extends StatefulWidget {
 
 class _PropertySearchFormState extends State<PropertySearchForm> {
   final _formKey = GlobalKey<FormState>();
+  final _PropertySearchDelegate _searchDelegate = _PropertySearchDelegate();
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
@@ -21,11 +22,13 @@ class _PropertySearchFormState extends State<PropertySearchForm> {
         children: <Widget>[
           Expanded(
             child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Enter a property', prefixIcon: Icon(Icons.search)),
-              readOnly: true,
-              //onTap: ,
-            ),
+                decoration: InputDecoration(
+                    hintText: 'Search property',
+                    prefixIcon: Icon(Icons.search)),
+                readOnly: true,
+                onTap: () {
+                  showSearchPage(context, _searchDelegate);
+                }),
           ),
           IconButton(
             onPressed: () {
@@ -43,6 +46,143 @@ class _PropertySearchFormState extends State<PropertySearchForm> {
           ),
         ],
       ),
+    );
+  }
+
+  //Shows Search result
+  void showSearchPage(
+      BuildContext context, _PropertySearchDelegate searchDelegate) async {
+    final String selected = await showSearch<String>(
+      context: context,
+      delegate: searchDelegate,
+    );
+
+    if (selected != null) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Searching: $selected'),
+        ),
+      );
+    }
+  }
+}
+
+class _PropertySearchDelegate extends SearchDelegate<String> {
+  final List<String> _suggestions = List();
+  final List<String> _histories = List();
+
+  _PropertySearchDelegate() {
+    _suggestions.add('2 BHK in Thane');
+    _suggestions.add('3 BHK in Mira Road');
+    //_histories.add('2 BHK in Thane');
+    //_histories.add('3 BHK in Mira Road');
+  }
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      query.isNotEmpty
+          ? IconButton(
+              tooltip: 'Clear',
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                query = '';
+                showSuggestions(context);
+              },
+            )
+          : IconButton(
+              icon: const Icon(Icons.mic),
+              tooltip: 'Voice input',
+              onPressed: () {
+                this.query = 'TBW: Get input from voice';
+              },
+            ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      tooltip: 'Back',
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        //Take control back to previous page
+        this.close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) => null;
+
+  void processResults(BuildContext context) {
+    if (this.query.isNotEmpty) {
+      _histories
+          .removeWhere((e) => e.toUpperCase() == this.query.toUpperCase());
+      _histories.insert(0, this.query);
+    }
+
+    this.close(context, this.query);
+  }
+
+  @override
+  void showResults(BuildContext context) {
+    processResults(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final Iterable<String> suggestions = this.query.isEmpty
+        ? _histories
+        : _suggestions
+            .where((word) => word.toUpperCase().contains(query.toUpperCase()));
+    return _PropertySuggestionWidget(
+      query: this.query,
+      suggestions: suggestions.toList(),
+      onSelected: (String suggestion) {
+        this.query = suggestion;
+        showResults(context);
+      },
+    );
+  }
+}
+
+class _PropertySuggestionWidget extends StatelessWidget {
+  const _PropertySuggestionWidget(
+      {this.suggestions, this.query, this.onSelected});
+
+  final List<String> suggestions;
+  final String query;
+  final ValueChanged<String> onSelected;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (BuildContext context, int i) {
+        final String suggestion = suggestions[i];
+        return ListTile(
+          leading: query.isEmpty ? Icon(Icons.history) : Icon(null),
+          title: RichText(
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: <TextSpan>[
+                TextSpan(
+                    text: suggestion.substring(0, query.length),
+                    // Highlight the substring that matched the query.
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(
+                  text: suggestion.substring(query.length),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            onSelected(suggestion);
+          },
+        );
+      },
     );
   }
 }
@@ -247,9 +387,6 @@ class _PropertySearchResultScreenState
     return chips;
   }
 }
-
-// oLD Code
-
 
 class PropertySearchDetailScreen extends StatelessWidget {
   final PropertyDetail propertyDetail;
